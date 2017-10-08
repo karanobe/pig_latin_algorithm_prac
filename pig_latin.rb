4dbfce37d0a73a67dee10c959ed5551c72dd89cf
Karan Oberoi <koberoi1017@gmail.com>
2017-10-08 11:35:03 -0700

    add test

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,1 +1,24 @@
 # Your code here
+#First step is were going to split the word we are given
+# Result will be an array
+#Annalyze the 0th index of the array
+#If the first letter is a vowel return the word
+#Else pop the first char to the last place of the word, and concatanate ay onto the end.
+
+def pig_latin_word(word)
+  array = word.split("")
+  if array[0].match(/[aeiou]/) != nil
+    word
+  else
+    array.insert(-1, array.delete_at(0))
+    array.join + "ay"
+  end
+end
+
+def pig_latin_sentence(string)
+  pig_sentence = []
+  sentence_array = string.downcase.split(" ")
+  sentence_array.each { |word| pig_sentence << pig_latin_word(word) }
+  pig_sentence.join(" ").capitalize
+end
+
--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -1,7 +1,15 @@
 require_relative('../pig_latin')
 
 describe 'Converting single words to Pig Latin' do
+  it 'converts cats to atscay' do
+  expect(pig_latin_word("cats")).to eq "atscay"
+  end
 end
 
+
 describe 'Converting a sentence to Pig Latin' do
+  it 'converts sentence to pig latin sentence' do
+  expect(pig_latin_sentence("This is a sentence")).to eq "Histay is a entencesay"
+  end
 end
+
